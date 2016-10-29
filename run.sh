ae00e74dae1186353d802c30324cedb83a460d3a
Xiang Ren <xren7@illinois.edu>
2016-10-29 08:18:23 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -11,20 +11,11 @@ echo 'Step 1 Generate Features'
 python code/DataProcessor/feature_generation.py $Data 10 0 1.0
 echo ' '
 
-### Train ReType on Joint Classification
-echo 'Step 2 Train ReType on Joint Classification'
-code/Model/retype/retype-rm -data $Data -mode m -size 50 -negative 5 -threads 40 -alpha 0.0001 -samples 1 -iters 400 -lr 0.02
+### Train ReType on Relation Classification
+echo 'Step 2 Train ReType on Relation Classification'
+code/Model/retype/retype -data $Data -mode j -size 50 -negative 5 -threads 20 -alpha 0.0001 -samples 1 -iters 100 -lr 0.025 -transWeight 7.0
 
-### Evaluate ReType on Joint Classification
-echo 'Step 3 Evaluate ReType on Joint Classification'
-python code/Evaluation/emb_test.py $Data retypeRm cosine 0.0
+### Evaluate ReType on Relation Classification
+echo 'Step 3 Evaluate ReType on Relation Classification'
+python code/Evaluation/emb_test.py $Data retype cosine 0.0
 echo ' '
-
-### Train ReType on Entity Classfication
-echo 'Step 4 Train ReType on Entity Classification'
-code/Model/retype/retype -data $Data -mode j -size 50 -negative 5 -alpha 0.0001 -samples 1 -threads 35 -iters 30 -lr 0.025 -transWeight 0.0
-echo ' '
-
-### Evaluate ReType on Entity Classification
-echo 'Step 5 Evaluate ReType in Entity Classification'
-python code/Evaluation/emb_test_em.py $Data retypeRm cosine 0.0
