19b72b1fe61080c71d9176e1385b5b55db27bbab
shanzhenren <renxiangzju@gmail.com>
2017-05-25 08:29:35 -0500

    Update

--- run.sh
+++ run.sh
@@ -12,9 +12,9 @@ python code/DataProcessor/feature_generation.py $Data 10 0 1.0
 echo ' '
 
 ### Train ReType on Relation Classification
-### - KBP: -negative 3 -lr 0.02 -transWeight 1.0
-###	- NYT: -negative 3 -lr 0.02 -transWeight 1.0
-### - BioInfer: -negative 3 -lr 0.02 -transWeight 1.0
+### - KBP: -negative 3 -iters 400 -lr 0.02 -transWeight 1.0
+###	- NYT: -negative 5 -iters 700 -lr 0.02 -transWeight 7.0
+### - BioInfer: -negative 5 -iters 700 -lr 0.02 -transWeight 7.0
 echo 'Learn CoType embeddings...'
 code/Model/retype/retype -data $Data -mode j -size 50 -negative 3 -threads 35 -alpha 0.0001 -samples 1 -iters 400 -lr 0.02 -transWeight 1.0
 echo ' '
